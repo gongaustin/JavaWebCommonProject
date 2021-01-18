fdc8e0b9f6df985877ae73cc6fba9b28882ecab2
GongJun <28402674@qq.com>
2021-01-18 16:26:04 +0800

    SpringBootApplication源代码学习

--- src/main/java/com/austin/common/CommonApplicationStarter.java
+++ src/main/java/com/austin/common/CommonApplicationStarter.java
@@ -2,12 +2,15 @@ package com.austin.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Description:启动器Starter
  * @Author: GongJun
  * @Date: Created in 11:16 2021/1/18
  * */
+//@SpringBootApplication已经包括@ComponentScan和@EnableAutoConfiguration,单独写出来声明Package后则@SpringBootApplication中的就会失效
+@ComponentScan({"com.austin.common.mapper"})
 @SpringBootApplication
 public class CommonApplicationStarter {
     public static void main(String[] args) {
