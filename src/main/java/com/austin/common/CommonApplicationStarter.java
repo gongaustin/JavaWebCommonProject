c91b39a7e0cd948119fd4750cc63cee4e21db94c
GongJun <28402674@qq.com>
2021-01-18 14:53:35 +0800

    初始代码提交

---
+++ src/main/java/com/austin/common/CommonApplicationStarter.java
@@ -0,0 +1,16 @@
+package com.austin.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: GongJun
+ * @Date: Created in 11:16 2021/1/18
+ * */
+@SpringBootApplication
+public class CommonApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplicationStarter.class);
+    }
+}
